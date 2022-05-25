90111c305ddb80abf0fceedb21d0f3c7516efb5d
Godwin Aden <godwinaden@gmail.com>
2022-05-25 01:07:51 +0100

    more repos

--- src/database/repositories/cinema_repo.ts
+++ src/database/repositories/cinema_repo.ts
@@ -1,20 +1,33 @@
 
 
 // creating a custom type for any json object
-type JSONValue =
-    | string
-    | number
-    | boolean
-    | { [x: string]: JSONValue }
-    | Array<JSONValue>;
+import {Cinema} from "../db_models/cinema";
 
+export type AnyJSON = {
+    [key: string]: | string
+        | number
+        | boolean
+        | { [key: string]: AnyJSON }
+        | Array<AnyJSON>;
+}
 
 class CinemaRepo{
-    json: JSONValue = {
-        full_name: "Username"
-    };
 
-    createCinema(cinema: JSONValue){
+    async create_cinema(cinema_data: AnyJSON): Promise<Cinema | Error>{
+        try{
+            return await Cinema.create({
+                brand_name: cinema_data['brand_name'],
+                country: cinema_data['country'],
+                city: cinema_data['city']
+            }, {fields: ['brand_name', 'country', 'city']});
+        }catch (err: any) {
+            return err;
+        }
+    }
+
+    get_cinemas_with_shows(){
 
     }
+
+    get_cinema_with_shows(cinema_id: string){}
 }
