626276ecfea120569116a73c39c05a3ed2b4a144
Godwin Aden <godwinaden@gmail.com>
2022-05-25 02:21:58 +0100

    more repos

--- src/database/repositories/cinema_repo.ts
+++ src/database/repositories/cinema_repo.ts
@@ -1,7 +1,8 @@
 
 
 // creating a custom type for any json object
-import {Cinema} from "../db_models/cinema";
+import {Cinema, Show} from "../db_models/cinema";
+import {Movie} from "../db_models/movie";
 
 export type AnyJSON = {
     [key: string]: | string
@@ -13,7 +14,7 @@ export type AnyJSON = {
 
 class CinemaRepo{
 
-    async create_cinema(cinema_data: AnyJSON): Promise<Cinema | Error>{
+    async create_cinema(cinema_data: AnyJSON): Promise<Cinema>{
         try{
             return await Cinema.create({
                 brand_name: cinema_data['brand_name'],
@@ -21,13 +22,75 @@ class CinemaRepo{
                 city: cinema_data['city']
             }, {fields: ['brand_name', 'country', 'city']});
         }catch (err: any) {
-            return err;
+            throw new Error(err.toString());
         }
     }
 
-    get_cinemas_with_shows(){
-
+    async get_cinemas_with_shows(limit: number, offset: number): Promise<Cinema[]> {
+        try{
+            return await Cinema.findAll({
+                where: {
+                    status: true
+                },
+                order: [
+                    ['createdAt', 'DESC'],
+                    [Show, Movie, 'createdAt', 'DESC'],
+                ],
+                offset: offset, limit: limit
+            });
+        } catch(err: any){
+            throw new Error(err.toString());
+        }
     }
 
-    get_cinema_with_shows(cinema_id: string){}
+    async get_cinema_with_shows(cinema_id: number, limit: number, offset: number){
+        try{
+            return await Cinema.findOne({
+                where: {
+                    id: cinema_id,
+                    status: true
+                },
+                order: [
+                    ['createdAt', 'DESC'],
+                    [Show, Movie, 'createdAt', 'DESC'],
+                ],
+                offset: offset, limit: limit
+            });
+        } catch(err: any){
+            throw new Error(err.toString());
+        }
+    }
+
+    async delete_cinema(cinema_id: number){
+        try{
+            return await Cinema.destroy({
+                where: {
+                    id: cinema_id
+                }
+            });
+        } catch(err: any){
+            throw new Error(err.toString());
+        }
+    }
+
+    async update_cinema(cinema_id: number, update_data: AnyJSON){
+        try{
+            return await Cinema.update(update_data,{
+                where: {
+                    id: cinema_id
+                },
+                fields: ["country", 'city', "brand_name"]
+            });
+        } catch(err: any){
+            throw new Error(err.toString());
+        }
+    }
+
+    async rate_cinema(cinema_id: number, rate: number){
+        try{
+            return await Cinema.increment({votes: rate, voters: 1}, { where: { id: cinema_id } });
+        } catch(err: any){
+            throw new Error(err.toString());
+        }
+    }
 }
